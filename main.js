4f97e99c1c1356cb3cd15ca97bbf6105361dbe0a
Jason <jljasonlin1101@gmail.com>
2023-07-05 16:44:23 -0400

    Forgot stuff

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 let newName = document.getElementById("Name");
 let aboutMe = document.getElementById("aboutMe");
 let history = document.getElementById("historySub");
-
-/*const createName = () => {
-    let nameLength = newName.innerHTML.length;
-    let currentName = newName.innerHTML;
-    newName.innerHTML = "J";
-    for(let i = 1; i < nameLength; i++){
-        setTimeout(() => {newName.innerHTML = newName.innerHTML + currentName[i];}, (500 * i))
-    }
-    
-}*/
+let skills = document.getElementById("skillsSub");
 
 const createName = (event) => {
     let nameLength = event.innerHTML.length;
     let currentName = event.innerHTML;
-    event.innerHTML = "";
+    event.innerHTML = " ";
     for(let i = 0; i < nameLength; i++){
         setTimeout(() => {event.innerHTML = event.innerHTML + currentName[i];}, (300 * i))
     }
     
 }
 
-
+console.log(location.pathname.split("/").slice(-1));
 
 //createName(newName);
-document.addEventListener('load', createName(newName));
-document.addEventListener('load', createName(aboutMe));
-document.addEventListener('load', createName(history));
+if(location.pathname.split("/").slice(-1)[0] === "index.html"){
+    document.addEventListener('load', createName(newName));
+    document.addEventListener('load', createName(aboutMe));
+    document.addEventListener('load', createName(history));
+    document.addEventListener('load', createName(skills));
+}
+
+if(location.pathname.split("/").slice(-1)[0] === "contacts.html"){
+    document.addEventListener('load', createName(newName));
+}
